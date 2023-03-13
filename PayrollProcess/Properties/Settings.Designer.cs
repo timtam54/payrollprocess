d03bf9fbe63c586a989bc6b9f717f2c1056b7bb0
Catherine Naum <timtam54@hotmail.com>
2023-03-13 11:54:03 +1000

    first commit

--- PayrollProcess/Properties/Settings.Designer.cs
+++ PayrollProcess/Properties/Settings.Designer.cs
@@ -27,7 +27,7 @@ namespace PayrollProcess.Properties {
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
         [global::System.Configuration.DefaultSettingValueAttribute("Data Source=sql2014;Initial Catalog=CTRCTSPayrollDB;Integrated Security=False;Con" +
-            "nect Timeout=30;UID=PayrollApp;PWD=RQkTuemu;")]
+            "nect Timeout=30;UID=payrollDB;PWD=Payroll2023;")]
         public string CTRCTSPayrollDBConnectionString1 {
             get {
                 return ((string)(this["CTRCTSPayrollDBConnectionString1"]));
